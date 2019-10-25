23ddd7e67affd63253f1319046998a9c397bacef
Yunior Almengo <yalmengo@intellisys.com.do>
2019-10-25 16:01:16 -0400

    Updates

---
+++ server/main.js
@@ -0,0 +1,375 @@
+require("source-map-support").install();
+module.exports =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// object to store loaded chunks
+/******/ 	// "0" means "already loaded"
+/******/ 	var installedChunks = {
+/******/ 		1: 0
+/******/ 	};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/ 	// This file contains only the entry chunk.
+/******/ 	// The chunk loading function for additional chunks
+/******/ 	__webpack_require__.e = function requireEnsure(chunkId) {
+/******/ 		// "0" is the signal for "already loaded"
+/******/ 		if(installedChunks[chunkId] !== 0) {
+/******/ 			var chunk = require("./" + ({"0":"home"}[chunkId]||chunkId) + "-" + {"0":"023843ca346b2f82357b"}[chunkId] + ".js");
+/******/ 			var moreModules = chunk.modules, chunkIds = chunk.ids;
+/******/ 			for(var moduleId in moreModules) {
+/******/ 				modules[moduleId] = moreModules[moduleId];
+/******/ 			}
+/******/ 			for(var i = 0; i < chunkIds.length; i++)
+/******/ 				installedChunks[chunkIds[i]] = 0;
+/******/ 		}
+/******/ 		return Promise.resolve();
+/******/ 	};
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/ 	// uncatched error handler for webpack runtime
+/******/ 	__webpack_require__.oe = function(err) {
+/******/ 		process.nextTick(function() {
+/******/ 			throw err; // catch this error by using System.import().catch()
+/******/ 		});
+/******/ 	};
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+module.exports = require("react");
+
+/***/ }),
+/* 1 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+
+// CONCATENATED MODULE: ./src/server/template.js
+/* eslint-disable prefer-template, max-len */
+
+const getDeferScript = src => src ? `<script defer src="${src}"></script>` : '';
+
+/* harmony default export */ var template_defaultExport = (vo => `
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+    <meta charSet='utf-8' />
+    <meta httpEquiv="Content-Language" content="en" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link id="favicon" rel="shortcut icon" href="/kyt-favicon.png" sizes="16x16 32x32" type="image/png" />
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css">
+    ${vo.mainCSSBundle ? '<link rel="stylesheet" type="text/css" href="' + vo.mainCSSBundle + '">' : ''}
+
+    <title>Universal React Starter Kyt</title>
+  </head>
+
+  <body>
+    <div id="root"><div>${vo.root}</div></div>
+    ${getDeferScript(vo.manifestJSBundle)}
+    ${getDeferScript(vo.vendorJSBundle)}
+    ${getDeferScript(vo.mainJSBundle)}
+  </body>
+
+</html>
+
+`);
+// CONCATENATED MODULE: ./src/components/App/index.js
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_react__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_react_router_lib_Link__ = __webpack_require__(11);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_react_router_lib_Link___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_react_router_lib_Link__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__styles_scss__ = __webpack_require__(12);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__styles_scss___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2__styles_scss__);
+
+
+
+
+function App({ children }) {
+  return __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+    'div',
+    null,
+    children
+  );
+}
+
+App.propTypes = {
+  children: __WEBPACK_IMPORTED_MODULE_0_react__["PropTypes"].node.isRequired
+};
+
+/* harmony default export */ var App_defaultExport = (App);
+// CONCATENATED MODULE: ./src/routes/index.js
+/* harmony import */ var routes___WEBPACK_IMPORTED_MODULE_0_react__ = __webpack_require__(0);
+/* harmony import */ var routes___WEBPACK_IMPORTED_MODULE_0_react___default = __webpack_require__.n(routes___WEBPACK_IMPORTED_MODULE_0_react__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_react_router_lib_Route__ = __webpack_require__(9);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_react_router_lib_Route___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_react_router_lib_Route__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_react_router_lib_IndexRoute__ = __webpack_require__(10);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_react_router_lib_IndexRoute___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_react_router_lib_IndexRoute__);
+
+
+
+
+
+// Webpack 2 supports ES2015 `import()` by auto-
+// chunking assets. Check out the following for more:
+// https://webpack.js.org/guides/migrating/#code-splitting-with-es2015
+
+const importHome = (nextState, cb) => {
+  __webpack_require__.e/* import() */(0).then(__webpack_require__.bind(null, 18)).then(module => cb(null, module.default)).catch(e => {
+    throw e;
+  });
+};
+
+// We use `getComponent` to dynamically load routes.
+// https://github.com/reactjs/react-router/blob/master/docs/guides/DynamicRouting.md
+const routes = routes___WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+  __WEBPACK_IMPORTED_MODULE_1_react_router_lib_Route___default.a,
+  { path: '/', component: App_defaultExport },
+  routes___WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_2_react_router_lib_IndexRoute___default.a, { getComponent: importHome })
+);
+
+// Unfortunately, HMR breaks when we dynamically resolve
+// routes so we need to require them here as a workaround.
+// https://github.com/gaearon/react-hot-loader/issues/288
+if (false) {
+  require('../components/Home'); // eslint-disable-line global-require
+}
+
+/* harmony default export */ var routes_defaultExport = (routes);
+// CONCATENATED MODULE: ./src/server/index.js
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_express__ = __webpack_require__(2);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_express___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_express__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_compression__ = __webpack_require__(3);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_compression___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_compression__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_path__ = __webpack_require__(4);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_path___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_path__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_react__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_3_react__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_react_dom_server__ = __webpack_require__(5);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_react_dom_server___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_4_react_dom_server__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5_react_router_lib_RouterContext__ = __webpack_require__(6);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5_react_router_lib_RouterContext___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_5_react_router_lib_RouterContext__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6_react_router_lib_createMemoryHistory__ = __webpack_require__(7);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6_react_router_lib_createMemoryHistory___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_6_react_router_lib_createMemoryHistory__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7_react_router_lib_match__ = __webpack_require__(8);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7_react_router_lib_match___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_7_react_router_lib_match__);
+
+
+
+
+
+
+
+
+
+
+
+
+const clientAssets = __webpack_require__(13); // eslint-disable-line import/no-dynamic-require
+const port = parseInt("3000", 10);
+const app = __WEBPACK_IMPORTED_MODULE_0_express___default.a();
+
+// Remove annoying Express header addition.
+app.disable('x-powered-by');
+
+// Compress (gzip) assets in production.
+app.use(__WEBPACK_IMPORTED_MODULE_1_compression___default.a());
+
+// Setup the public directory so that we can server static assets.
+app.use(__WEBPACK_IMPORTED_MODULE_0_express___default.a.static(__WEBPACK_IMPORTED_MODULE_2_path___default.a.join(process.cwd(), "build/public")));
+
+// Setup server side routing.
+app.get('*', (request, response) => {
+  const history = __WEBPACK_IMPORTED_MODULE_6_react_router_lib_createMemoryHistory___default.a(request.originalUrl);
+
+  __WEBPACK_IMPORTED_MODULE_7_react_router_lib_match___default.a({ routes: routes_defaultExport, history }, (error, redirectLocation, renderProps) => {
+    if (error) {
+      response.status(500).send(error.message);
+    } else if (redirectLocation) {
+      response.redirect(302, `${redirectLocation.pathname}${redirectLocation.search}`);
+    } else if (renderProps) {
+      // When a React Router route is matched then we render
+      // the components and assets into the template.
+      response.status(200).send(template_defaultExport({
+        root: __WEBPACK_IMPORTED_MODULE_4_react_dom_server__["renderToString"](__WEBPACK_IMPORTED_MODULE_3_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_5_react_router_lib_RouterContext___default.a, renderProps)),
+        manifestJSBundle: clientAssets['manifest.js'],
+        mainJSBundle: clientAssets['main.js'],
+        vendorJSBundle: clientAssets['vendor.js'],
+        mainCSSBundle: clientAssets['main.css']
+      }));
+    } else {
+      response.status(404).send('Not found');
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`✅  server started on port: ${port}`); // eslint-disable-line no-console
+});
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports) {
+
+module.exports = require("express");
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports) {
+
+module.exports = require("compression");
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports) {
+
+module.exports = require("path");
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports) {
+
+module.exports = require("react-dom/server");
+
+/***/ }),
+/* 6 */
+/***/ (function(module, exports) {
+
+module.exports = require("react-router/lib/RouterContext");
+
+/***/ }),
+/* 7 */
+/***/ (function(module, exports) {
+
+module.exports = require("react-router/lib/createMemoryHistory");
+
+/***/ }),
+/* 8 */
+/***/ (function(module, exports) {
+
+module.exports = require("react-router/lib/match");
+
+/***/ }),
+/* 9 */
+/***/ (function(module, exports) {
+
+module.exports = require("react-router/lib/Route");
+
+/***/ }),
+/* 10 */
+/***/ (function(module, exports) {
+
+module.exports = require("react-router/lib/IndexRoute");
+
+/***/ }),
+/* 11 */
+/***/ (function(module, exports) {
+
+module.exports = require("react-router/lib/Link");
+
+/***/ }),
+/* 12 */
+/***/ (function(module, exports) {
+
+module.exports = {
+	"header": "styles-header--3RQKb",
+	"nav": "styles-nav--3RC0V",
+	"content": "styles-content--3jPwW",
+	"navItem": "styles-navItem--1_GGr",
+	"link": "styles-link--2D2uz",
+	"logo": "styles-logo--1fuKD",
+	"selected": "styles-selected--2DdJb"
+};
+
+/***/ }),
+/* 13 */
+/***/ (function(module, exports) {
+
+module.exports = {"home.js":"/home-c7b910d6a61e9d1cb9e2.js","home.js.map":"/home-c7b910d6a61e9d1cb9e2.js.map","icon-kytLogo_large-252x252.svg":"/icon-kytLogo_large-252x252-2d220d9ba0262a640a42a73814b493a6.svg","main.css":"/main-eccb12b033a0c0375208fb867c370517.css","main.css.map":"/main-eccb12b033a0c0375208fb867c370517.css.map","main.js":"/main-a8281085003b297a7e12.js","main.js.map":"/main-a8281085003b297a7e12.js.map","manifest.js":"/manifest-2cbacf07b766571feec9.js","manifest.js.map":"/manifest-2cbacf07b766571feec9.js.map","vendor.js":"/vendor-42c43ae0f80dedd8201d.js","vendor.js.map":"/vendor-42c43ae0f80dedd8201d.js.map"}
+
+/***/ }),
+/* 14 */,
+/* 15 */,
+/* 16 */,
+/* 17 */,
+/* 18 */,
+/* 19 */
+/***/ (function(module, exports) {
+
+module.exports = require("os");
+
+/***/ }),
+/* 20 */
+/***/ (function(module, exports) {
+
+module.exports = require("react-print-tool");
+
+/***/ })
+/******/ ]);
+//# sourceMappingURL=main.js.map
